a0f4ab2cbc5766fd82ca7e8d210af850592f7da7
chng <zhchngzng@gmail.com>
2015-03-25 19:11:35 -0700

    move packet format definition into AppPacketFormat.h!

---
+++ AppPacketFormat.h
@@ -0,0 +1,14 @@
+#define MSGTYPE_POLL "poll"
+#define MSGTYPE_WRMSG "wrmsg"
+#define MSGTYPE_NEWMSG "newmsg"
+
+#define LEN_MSG_POLL 55
+#define LEN_MSG_NEWMSG 4096
+#define LEN_MSG_WRMSG 4096
+
+#define LEN_MSGTYPE 10
+#define LEN_USERID 20
+#define LEN_USERNAME 20
+#define LEN_PASSWORD 20
+#define LEN_TEXT 4000
+#define LEN_MSG 4096
